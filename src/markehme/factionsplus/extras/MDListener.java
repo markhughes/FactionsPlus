b3a77d43fa3295d22cc7ff7a8fde126d8e424cba
Andrew Wen <2060andrew@gmail.com>
2012-06-20 02:25:55 -0700

    Fixed LWC Integration Offline Player Handling

--- src/markehme/factionsplus/extras/MDListener.java
+++ src/markehme/factionsplus/extras/MDListener.java
@@ -4,12 +4,15 @@ import markehme.factionsplus.FactionsPlus;
 import me.desmin88.mobdisguise.api.MobDisguiseAPI;
 import me.desmin88.mobdisguise.api.event.DisguiseEvent;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.FPlayers;
 
 public class MDListener implements Listener  {
+	@EventHandler(priority=EventPriority.HIGHEST)
 	public void onDisguiseEvent(DisguiseEvent e) {
 		FPlayer fplayer = FPlayers.i.get(e.getPlayer());
 		
@@ -17,7 +20,7 @@ public class MDListener implements Listener  {
         		if(fplayer.isInEnemyTerritory()) {
         			
         			MobDisguiseAPI.undisguisePlayer(e.getPlayer());
-        			e.getPlayer().sendMessage("You have been un-disguised!");
+        			e.getPlayer().sendMessage("You have been un-disguised due to being in enemy territory!");
         			e.setCancelled(true);
         		}
         	}
@@ -26,7 +29,7 @@ public class MDListener implements Listener  {
         		if(fplayer.isInOwnTerritory()) {
         			
         			MobDisguiseAPI.undisguisePlayer(e.getPlayer());
-        			e.getPlayer().sendMessage("You have been un-disguised!");
+        			e.getPlayer().sendMessage("You have been un-disguised due to being in your own territory!");
         			e.setCancelled(true);
         		}
         	}
