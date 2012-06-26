538b7ace521b91bae9495cd25d2de0cdf658b226
basicsensei <basicsensei@gmail.com>
2012-06-26 20:38:00 +0200

    making it more obvious & allowing op/perm to bypass LWC deny locking if not in wilderness or own faction land

--- src/markehme/factionsplus/extras/LWCListener.java
+++ src/markehme/factionsplus/extras/LWCListener.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.permissions.*;
 
 import com.griefcraft.scripting.event.LWCProtectionRegisterEvent;
 import com.massivecraft.factions.Board;
@@ -24,6 +25,7 @@ import markehme.factionsplus.extras.LWCFunctions;
 public class LWCListener implements Listener {
 	
 	private final static String	msg	= "internal error clearing LWC locks on land claim, inform admin to check console.";
+	private static final Permission	permForAllowLWCLocksAnywhere	= new Permission( "allowLWCLocksAnywhere" );
 	
 	
 	@EventHandler(
@@ -48,11 +50,11 @@ public class LWCListener implements Listener {
 		}
 	}
 	
-	@EventHandler
+	@EventHandler(ignoreCancelled=true)
 	public void onLWCProtectionRegister(LWCProtectionRegisterEvent event){
-		if(event.isCancelled()){
-			return;
-		}
+//		if(event.isCancelled()){
+//			return;
+//		}
 		
 		Player p = event.getPlayer();
 		Block b = event.getBlock();
@@ -60,12 +62,12 @@ public class LWCListener implements Listener {
 		FLocation floc = new FLocation(b.getLocation());
 		Faction owner = Board.getFactionAt(floc);
 
-		if(!owner.isNone() && owner != fp.getFaction()){
-			//if it's not wilderness && it's not player's own faction land
-			//then you can't create locks
-			//TODO: what about if it's safezone/warzone ? & should op be allowed to bypass this?
-			event.setCancelled(true);
-			fp.sendMessage("You cannot create locks in another faction's territory!");
+		if (owner.isNone() || Utilities.hasPermissionOrIsOp( p, permForAllowLWCLocksAnywhere ) || owner.equals( fp.getFaction() )) {
+			//allow locks if it's in wilderness, or is op or has perm, or is in own faction land
+			return;//the 'if' is easier to read this way
+		}else {
+			event.setCancelled( true );
+			fp.sendMessage( ChatColor.RED + "You can lock only within your faction or unclaimed land!" );
 		}
 	}
 }
