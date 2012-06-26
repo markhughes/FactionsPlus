cf75c43ec1fb73d6518210e60abc4921db0fb500
Sean Schwartz <unvcustoms@gmail.com>
2012-06-26 06:52:07 -0500

    Advanced version checking added to update checker.

--- src/markehme/factionsplus/FactionsPlusUpdate.java
+++ src/markehme/factionsplus/FactionsPlusUpdate.java
@@ -31,13 +31,36 @@ public class FactionsPlusUpdate {
 				scanner.close();
 			}
 		}
-		
-		if(!content.trim().equalsIgnoreCase(v.trim())) {
-			FactionsPlus.log.warning("! -=====================================- !");
-			FactionsPlus.log.warning("FactionsPlus has an update, you");
-			FactionsPlus.log.warning("can upgrade to version " + content.trim() + " via");
-			FactionsPlus.log.warning("http://dev.bukkit.org/server-mods/factionsplus/");
-			FactionsPlus.log.warning("! -=====================================- !");
+
+		// advanced checking
+		if(!content.trim().equalsIgnoreCase(v.trim())){
+			int web, current;
+			String tempWeb = content.trim().replace(".", "");
+			String tempThis = v.trim().replace(".", "");
+
+			web = Integer.parseInt(tempWeb);
+			current = Integer.parseInt(tempThis);
+			
+			// Check if version lengths are the same
+			if(tempWeb.length() == tempThis.length()){
+				if(web > current){
+					// Version lengths different, unable to advance compare
+					FactionsPlus.log.warning("! -=====================================- !");
+					FactionsPlus.log.warning("FactionsPlus has an update, you");
+					FactionsPlus.log.warning("can upgrade to version " + content.trim() + " via");
+					FactionsPlus.log.warning("http://dev.bukkit.org/server-mods/factionsplus/");
+					FactionsPlus.log.warning("! -=====================================- !");
+				} else {
+					FactionsPlus.info("Up to date!");
+				}
+			} else {
+				// Version lengths different, unable to advance compare
+				FactionsPlus.log.warning("! -=====================================- !");
+				FactionsPlus.log.warning("FactionsPlus has an update, you");
+				FactionsPlus.log.warning("can upgrade to version " + content.trim() + " via");
+				FactionsPlus.log.warning("http://dev.bukkit.org/server-mods/factionsplus/");
+				FactionsPlus.log.warning("! -=====================================- !");
+			}
 		} else {
 			FactionsPlus.info("Up to date!");
 		}
