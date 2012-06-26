ad7d96ec66b57bb2df4865023e2cd4d99a5187af
Sean Schwartz <unvcustoms@gmail.com>
2012-06-26 07:48:11 -0500

    Added LWC chest locking checks
    Made it check if player is attempting to lock a chest in land that isn't
    theirs/unclaimed.

--- src/markehme/factionsplus/extras/LWCListener.java
+++ src/markehme/factionsplus/extras/LWCListener.java
@@ -1,11 +1,19 @@
 package markehme.factionsplus.extras;
 
 import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
+import com.griefcraft.scripting.event.LWCProtectionRegisterEvent;
+import com.massivecraft.factions.Board;
+import com.massivecraft.factions.FLocation;
 import com.massivecraft.factions.FPlayer;
+import com.massivecraft.factions.FPlayers;
+import com.massivecraft.factions.Faction;
+import com.massivecraft.factions.Factions;
 import com.massivecraft.factions.event.LandClaimEvent;
 
 import markehme.factionsplus.*;
@@ -37,6 +45,23 @@ public class LWCListener implements Listener {
 				FactionsPlus.severe( msg );
 				fPlayer.sendMessage( FactionsPlus.FP_TAG_IN_LOGS + msg );
 			}
+		}
+	}
+	
+	@EventHandler
+	public void onLWCProtectionRegister(LWCProtectionRegisterEvent event){
+		if(event.isCancelled()){
+			return;
+		}
+		
+		Player p = event.getPlayer();
+		Block b = event.getBlock();
+		FPlayer fp = FPlayers.i.get(p);
+		FLocation floc = new FLocation(b.getLocation());
+		Faction owner = Board.getFactionAt(floc);
+
+		if(!owner.isNone() && owner != fp.getFaction()){
+			event.setCancelled(true);
 		}
 	}
 }
