c6ae8850391e267d47f66a608ea715d2d90f3e24
Andrew Wen <2060andrew@gmail.com>
2012-07-09 21:57:34 -0700

    Minimize impact on environment by only checking lava flow, this should be sufficient for most purposes.

--- src/markehme/factionsplus/listeners/LiquidFlowListener.java
+++ src/markehme/factionsplus/listeners/LiquidFlowListener.java
@@ -9,13 +9,13 @@ import org.bukkit.event.block.BlockFromToEvent;
 
 import com.massivecraft.factions.Board;
 import com.massivecraft.factions.FLocation;
-//Warning: REALLY high-resource use check that prevents water/lava flow between different faction owned territories (say good bye to cobble monsters)
+//Warning: REALLY high-resource use check that prevents lava flow between different faction owned territories (say good bye to cobble monsters)
 public class LiquidFlowListener implements Listener{
 	@EventHandler(priority=EventPriority.HIGHEST)
 	public static void onBlockFromTo(BlockFromToEvent event) {
 		Chunk fromchunk = event.getBlock().getChunk();
 		Chunk tochunk = event.getToBlock().getChunk();
-		if(!((event.getBlock().getTypeId() == 8)||(event.getBlock().getTypeId() == 9)||(event.getBlock().getTypeId() == 10)||(event.getBlock().getTypeId() == 11))) {
+		if(!((event.getBlock().getTypeId() == 10)||(event.getBlock().getTypeId() == 11))) {
 			return;
 		}
 		if(fromchunk==tochunk) {
