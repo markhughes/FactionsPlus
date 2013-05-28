e61b3abe5290ecfa4df7d0ccf4cfcf4c84ac0975
Mark Hughes <mark@round.net.au>
2013-05-28 22:41:39 +1000

    Chat deprecated.

--- src/markehme/factionsplus/Cmds/BaseCmdChatMode.java
+++ src/markehme/factionsplus/Cmds/BaseCmdChatMode.java
@@ -14,7 +14,6 @@ import com.massivecraft.factions.cmd.FCommand;
 @Deprecated
 public abstract class BaseCmdChatMode extends FPCommand {
 	
-	@Override
 	public final void performfp() {
 		return;
 	}
