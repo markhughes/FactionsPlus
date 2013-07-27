dbbec602cbaa1426751c5505a7e56ea5a4671286
Mark Hughes <mark@round.net.au>
2013-07-27 16:32:29 +1000

    New configuration option

--- src/markehme/factionsplus/config/sections/SubSection_Scoreboards.java
+++ src/markehme/factionsplus/config/sections/SubSection_Scoreboards.java
@@ -6,10 +6,15 @@ public class SubSection_Scoreboards {
 	@Option(
 			realAlias_inNonDottedFormat = "showScoreboardOfMap" )
 	public  final _boolean showScoreboardOfMap = new _boolean(false);
-	
+
 	@Option(
 			realAlias_inNonDottedFormat = "showScoreboardOfFactions" )
 	public  final _boolean showScoreboardOfFactions = new _boolean(true);
+
+	@Option(
+			realAlias_inNonDottedFormat = "sendScoreboardOnJoin" )
+	public  final _boolean sendScoreboardOnJoin = new _boolean(true);
+	
 	@Option(
 			realAlias_inNonDottedFormat = "secondsBetweenScoreboardUpdates" )
 	public  final _int secondsBetweenScoreboardUpdates = new _int(5);
