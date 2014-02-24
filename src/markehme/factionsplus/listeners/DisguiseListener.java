18a5631bda88028d649f6b8ec09f5e6c27557136
Mark Hughes <makr@markeh.me>
2014-02-25 03:06:39 +1100

    Adding support for new plugins, removing MobDisguise

--- src/markehme/factionsplus/listeners/DisguiseListener.java
+++ src/markehme/factionsplus/listeners/DisguiseListener.java
@@ -1,10 +1,15 @@
 package markehme.factionsplus.listeners;
 
+import java.util.Observer;
+
 import markehme.factionsplus.FactionsPlus;
 import markehme.factionsplus.config.Config;
 import markehme.factionsplus.references.FP;
 import markehme.factionsplus.references.FPP;
-import me.desmin88.mobdisguise.api.MobDisguiseAPI;
+import me.libraryaddict.disguise.disguisetypes.DisguiseType;
+import me.libraryaddict.disguise.disguisetypes.FlagWatcher;
+import me.libraryaddict.disguise.disguisetypes.PlayerDisguise;
+import me.libraryaddict.disguise.utilities.DisguiseUtilities;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -15,43 +20,38 @@ import org.bukkit.plugin.PluginManager;
 
 import com.massivecraft.factions.entity.UPlayer;
 
+import de.robingrether.idisguise.api.DisguiseAPI;
+
 import pgDev.bukkit.DisguiseCraft.DisguiseCraft;
 import pgDev.bukkit.DisguiseCraft.api.DisguiseCraftAPI;
 
-
 public class DisguiseListener implements Listener {
-	private static final String	MOB_DISGUISE	= "MobDisguise";
+	private static final String	IDISGUISE		= "iDisguise";
 	private static final String	DISGUISE_CRAFT	= "DisguiseCraft";
+	private static final String	LIBS_DISGUISE	= "LibsDisguises";
 
+	private static DisguiseListener	disguiselistener	= null;
+	
+	private static DCListener		dclistener			= null;
+	private static IDListener		idlistener 			= null;
+	private static LDListener		ldlistener 			= null;
+
+	
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent event) {
 		if(event.isCancelled()) {
 			return;
 		}
-
+		
 		UPlayer fplayer = UPlayer.get(event.getPlayer());
+		
 		if(!fplayer.hasFaction()) {
     		return;
     	}
 		
-		if(isMobDisguiseIntegrated()) {
-
-			if(MobDisguiseAPI.isDisguised(event.getPlayer())) {
-				if(Config._extras._disguise.unDisguiseIfInEnemyTerritory._) {
-					if(fplayer.isInEnemyTerritory()) {
-						MobDisguiseAPI.undisguisePlayer(event.getPlayer());
-						event.getPlayer().sendMessage("You have been un-disguised!");
-					}
-				}
-
-				if(Config._extras._disguise.unDisguiseIfInOwnTerritory._) {
-					if(fplayer.isInOwnTerritory()) {
-						MobDisguiseAPI.undisguisePlayer(event.getPlayer());
-						event.getPlayer().sendMessage("You have been un-disguised!");
-					}
-				}
-			}
-		}
+		/*
+		 *  Does a check with DisguiseCraft 
+		 */
 
 		if(isDisguiseCraftIntegrated()) {
 			DisguiseCraftAPI dcAPI = DisguiseCraft.getAPI();
@@ -72,42 +72,104 @@ public class DisguiseListener implements Listener {
 				}
 			}
 		}
+		
+		/*
+		 * Does a check with iDisguise 
+		 */
+		
+		if(isiDisguiseIntegrated()) {
+			
+			DisguiseAPI api = FP.server.getServicesManager().getRegistration(DisguiseAPI.class).getProvider();
+			
+			if(api.isDisguised(event.getPlayer())) {
+				if(Config._extras._disguise.unDisguiseIfInEnemyTerritory._) {
+					if(fplayer.isInEnemyTerritory()) {
+						api.undisguiseToAll(event.getPlayer());
+						event.getPlayer().sendMessage("You have been un-disguised!");
+					}
+				}
+
+				if(Config._extras._disguise.unDisguiseIfInOwnTerritory._) {
+					if(fplayer.isInOwnTerritory()) {
+						api.undisguiseToAll(event.getPlayer());
+						event.getPlayer().sendMessage("You have been un-disguised!");
+					}
+				}
+			}
+		}
+		
+		
+		/*
+		 * Does a check with Libs' Disguise 
+		 */
+		
+		if(isLibsDisguiseIntegrated()) {
+			
+			if(DisguiseUtilities.getDisguise(event.getPlayer(), event.getPlayer().getEntityId()) != null) {
+
+				if(Config._extras._disguise.unDisguiseIfInEnemyTerritory._) {
+					if(fplayer.isInEnemyTerritory()) {
+						me.libraryaddict.disguise.DisguiseAPI.undisguiseToAll(event.getPlayer());
+						
+						event.getPlayer().sendMessage("You have been un-disguised!");
+					}
+				}
+
+				if(Config._extras._disguise.unDisguiseIfInOwnTerritory._) {
+					if(fplayer.isInOwnTerritory()) {
+						me.libraryaddict.disguise.DisguiseAPI.undisguiseToAll(event.getPlayer());
+						event.getPlayer().sendMessage("You have been un-disguised!");
+					}
+				}
+			}
+		}
 
 
 	}
-
 	
-	private static DisguiseListener	disguiselistener	= null;
-	private static DCListener		dclistener			= null;
-	private static MDListener		mdlistener 			= null;
-
-	public final static boolean isMobDisguiseIntegrated() {
-		return null != mdlistener;
+	public final static boolean isiDisguiseIntegrated() {
+		return null != idlistener;
 	}
 
 	public final static boolean isDisguiseCraftIntegrated() {
 		return null != dclistener;
 	}
+	
+	public final static boolean isLibsDisguiseIntegrated() {
+		return null != ldlistener;
+	}
 
 	private final static void disintegrateDC() {
-//		assert null != dclistener:"bad call: DC was not already integrated";
 		if ( null != dclistener ) {
+			
 			HandlerList.unregisterAll( dclistener );
 			dclistener = null;
-			FP.info( "DC is disintegrated" );
-			if (!isMobDisguiseIntegrated()) {
+			FP.info( "iDisguise is disintegrated" );
+			if (!isiDisguiseIntegrated()) {
+				disintegrateCommon();
+			}
+			
+		}
+	}
+	
+	
+	private final static void disintegrateiD() {
+		if (null != idlistener) {
+			HandlerList.unregisterAll( idlistener );
+			idlistener = null;
+			FP.info( "iDisguise is disintegrated" );
+			if (!isDisguiseCraftIntegrated()) {
 				disintegrateCommon();
 			}
 		}
 	}
 	
-	
-	private final static void disintegrateMD() {
-		if (null != mdlistener) {
-			HandlerList.unregisterAll( mdlistener );
-			mdlistener = null;
-			FP.info( "MD is disintegrated" );
-			if (!isDisguiseCraftIntegrated()) {
+	private final static void disintegrateLD() {
+		if (null != ldlistener) {
+			HandlerList.unregisterAll( ldlistener );
+			ldlistener = null;
+			FP.info( "Lib's Disguise is disintegrated" );
+			if (!isLibsDisguiseIntegrated()) {
 				disintegrateCommon();
 			}
 		}
@@ -137,45 +199,64 @@ public class DisguiseListener implements Listener {
         	
 			PluginManager pm = Bukkit.getServer().getPluginManager();
 			
-			//we allow both MD and DC to be enabled, but the chances are only DC is.
 			
+			
+			// Attempt to setup DisguiseCraft
 			boolean isDCplugin=pm.isPluginEnabled(DISGUISE_CRAFT);
 			if ( isDCplugin && !isDisguiseCraftIntegrated() ) {
-				// not already integrated? do it now
 				assert ( null == dclistener );
 				dclistener = new DCListener();
 				pm.registerEvents( dclistener, instance );
 				ensureCommonIsAllocated( instance );
 				FPP.info( "Hooked into "+DISGUISE_CRAFT+"!" );
-			} else
-				if ( !isDCplugin && isDisguiseCraftIntegrated() ) {
-					// either the plugin went offline or the configs changed, we need to disable integration:
-					disintegrateDC();
-				}
+			} else if ( !isDCplugin && isDisguiseCraftIntegrated() ) {
+				// either the plugin unloaded or the configs changed, we need to disable integration:
+				disintegrateDC();
+			}
         	
-			boolean isMDplugin = pm.isPluginEnabled( MOB_DISGUISE );
-			if ( isMDplugin && !isMobDisguiseIntegrated() ) {
-				assert ( null == mdlistener );
-				mdlistener = new MDListener();
-				pm.registerEvents( mdlistener, instance );
+			// Attempt to setup iDisguise 
+			boolean isiDplugin = pm.isPluginEnabled( IDISGUISE );
+			if ( isiDplugin && !isiDisguiseIntegrated() ) {
+				assert ( null == idlistener );
+				idlistener = new IDListener();
+				pm.registerEvents( idlistener, instance );
 				ensureCommonIsAllocated( instance );
-				FPP.info( "Hooked into "+MOB_DISGUISE+"!" );
-			} else
-				if ( !isMDplugin && isMobDisguiseIntegrated() ) {
-					disintegrateMD();
-				}
-
-        	if (!isDCplugin && !isMDplugin) {//if neither plugin exists (we know the options exists though)
-        		FPP.warn("MobDisguise or DisguiseCraft integration enabled in config, but " +
-        				"none of these plugins are installed!");
-        	}
-        }else{
+				FPP.info( "Hooked into "+IDISGUISE+"!" );
+			} else if ( !isiDplugin && isiDisguiseIntegrated() ) {
+					disintegrateiD();
+			}
+			
+			// Attempt to setup Lib's Disguise
+			boolean isLDplugin=pm.isPluginEnabled(LIBS_DISGUISE);
+			if ( isLDplugin && !isLibsDisguiseIntegrated() ) {
+				assert ( null == ldlistener );
+				ldlistener = new LDListener();
+				pm.registerEvents( ldlistener, instance );
+				ensureCommonIsAllocated( instance );
+				FPP.info( "Hooked into "+LIBS_DISGUISE+"!" );
+			} else if ( !isLDplugin && isLibsDisguiseIntegrated() ) {
+				disintegrateLD();
+			}
+			
+			// Now, confirm one of them at least existed
+				
+	        if (!isDCplugin && !isiDplugin && !isLDplugin) {//if neither plugin exists (we know the options exists though)
+	        	FPP.warn("DisguiseCraft, iDisguise, or LibsDisguises integration enabled in config, but " +
+	        				"none of these plugins are installed!");
+	        
+	        }
+	        
+		} else {
         	if (isDisguiseCraftIntegrated()) {
         		disintegrateDC();
         	}
         	
-        	if (isMobDisguiseIntegrated()) {
-        		disintegrateMD();
+        	if (isiDisguiseIntegrated()) {
+        		disintegrateiD();
+        	}
+        	
+        	if (isLibsDisguiseIntegrated()) {
+        		disintegrateLD();
         	}
         }
 	}
