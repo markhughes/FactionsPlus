b8bb500f2471f852a2468a6461590013aa072f11
MarkehMe <mark@markeh.me>
2014-06-13 22:41:58 +1000

    MCore support, better method to support LConf

--- src/markehme/factionsplus/Cmds/CmdFactionNeed.java
+++ src/markehme/factionsplus/Cmds/CmdFactionNeed.java
@@ -2,8 +2,9 @@ package markehme.factionsplus.Cmds;
 
 import markehme.factionsplus.FactionsPlus;
 import markehme.factionsplus.FactionsPlusTemplates;
-import markehme.factionsplus.config.Config;
-import markehme.factionsplus.references.FP;
+import markehme.factionsplus.MCore.LConf;
+import markehme.factionsplus.config.OldConfig;
+import markehme.factionsplus.util.FPPerm;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -12,7 +13,9 @@ import org.bukkit.entity.Player;
 import com.massivecraft.factions.Rel;
 import com.massivecraft.factions.cmd.req.ReqFactionsEnabled;
 import com.massivecraft.factions.entity.UPlayer;
+import com.massivecraft.mcore.cmd.req.ReqHasPerm;
 import com.massivecraft.mcore.cmd.req.ReqIsPlayer;
+import com.massivecraft.mcore.util.Txt;
 
 public class CmdFactionNeed extends FPCommand {
 	
@@ -26,23 +29,16 @@ public class CmdFactionNeed extends FPCommand {
 		this.addRequirements(ReqFactionsEnabled.get());
 		this.addRequirements(ReqIsPlayer.get());
 		
-		this.setHelp("announces to Faction Leaders, and Officers that you're in need of a Faction.");
-		this.setDesc("announces to Faction Leaders, and Officers that you're in need of a Faction.");
+		this.addRequirements(ReqHasPerm.get(FPPerm.NEED.node));
+		
+		this.setHelp(LConf.get().cmdDescFactionNeed);
+		this.setDesc(LConf.get().cmdDescFactionNeed);
 	}
 	
 	@Override
-	public void performfp() {
-		if( ! FactionsPlus.permission.has( usender.getPlayer(), "factionsplus.need" ) ) {
-			
-			sender.sendMessage( ChatColor.RED + "No permission!" );
-			
-			return;
-			
-		}
-		
-		if( usender.hasFaction() ) {
-			msg( ChatColor.RED + "You already have a Faction!" );
-			
+	public void performfp() {		
+		if(usender.hasFaction()) {
+			msg(Txt.parse(LConf.get().factionNeedAlreadyHaveFaction));	
 			return;
 		}
 		
@@ -50,22 +46,23 @@ public class CmdFactionNeed extends FPCommand {
 		// TODO: Cooldown? 
 		// TODO: Allow certain Factions to ignore these messages ( /f needs ignore | /f needs listen )
 		
-		String[] argsa = { sender.getName() };
+		int i = 0;
 		
-        for ( Player p : Bukkit.getServer().getOnlinePlayers() ) {
-        	
-        	if( UPlayer.get(p).getRole() == Rel.LEADER || UPlayer.get(p).getRole() == Rel.OFFICER ) {
-        		
-        		if( ! FP.permission.has( p, "factionsplus.ignoreneeds" ) ) {
-        			
-        			p.sendMessage(  FactionsPlusTemplates.Go( "faction_need", argsa ).replace("!1", sender.getName() ) );
-        			
-        		}
-        	}	
-
-        }
-        
-        // Notify player that they recieved the need request! 
-        msg(Config._templates.factions_notified_need._);
+		for(Player p : Bukkit.getServer().getOnlinePlayers()) {
+			if(UPlayer.get(p).getRole() == Rel.LEADER || UPlayer.get(p).getRole() == Rel.OFFICER) {
+				if(!FactionsPlus.permission.has(p, "factionsplus.ignoreneeds")) {        			
+					i++;
+					p.sendMessage(Txt.parse(LConf.get().factionNeedNotification, sender.getName(), sender.getName()));
+				}
+			}
+		}
+		
+		if(i == 0) {
+			// Notify the player that no one received the need request
+			msg(Txt.parse(LConf.get().factionNeedClarifyNoneSent));
+		} else {
+			// Notify player that they received the need request
+			msg(Txt.parse(LConf.get().factionNeedClarifySent));
+		}
 	}
 }
