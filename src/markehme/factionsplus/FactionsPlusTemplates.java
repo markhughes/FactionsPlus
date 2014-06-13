83fbc87d0c2246dcaa901a30f2a5d26b7714846f
MarkehMe <mark@markeh.me>
2014-06-13 22:05:56 +1000

    Deprecated and changed to OldConfig

--- src/markehme/factionsplus/FactionsPlusTemplates.java
+++ src/markehme/factionsplus/FactionsPlusTemplates.java
@@ -1,63 +1,64 @@
 package markehme.factionsplus;
 
-import markehme.factionsplus.config.Config;
+import markehme.factionsplus.config.OldConfig;
 
 import org.bukkit.ChatColor;
 
+@Deprecated
 public class FactionsPlusTemplates {
 	public static String Go(String templateOption, String args[]) {
 		String workingstring = "Invalid Template File for " + templateOption;
 		
 		if(templateOption == "announcement_message") {
-			workingstring = Config._templates.announcement_message._;
+			workingstring = OldConfig._templates.announcement_message._;
 		}
 		
 		if(templateOption == "warp_created") {
-			workingstring = Config._templates.warp_created._;
+			workingstring = OldConfig._templates.warp_created._;
 		}
 		
 		if(templateOption == "notify_warp_created") {
-			workingstring = Config._templates.notify_warp_created._;
+			workingstring = OldConfig._templates.notify_warp_created._;
 		}
 		
 		if(templateOption == "jailed_message") {
-			workingstring = Config._templates.jailed_message._;
+			workingstring = OldConfig._templates.jailed_message._;
 		}
 		
 		if(templateOption == "warp_non_existant") {
-			workingstring = Config._templates.warp_non_existant._;
+			workingstring = OldConfig._templates.warp_non_existant._;
 		}
 		
 		if(templateOption == "warped_to") {
-			workingstring = Config._templates.warped_to._;
+			workingstring = OldConfig._templates.warped_to._;
 		}
 		
 		if(templateOption == "warped_removed") {
-			workingstring = Config._templates.warped_removed._;
+			workingstring = OldConfig._templates.warped_removed._;
 		}
 		
 		if(templateOption == "warp_incorrect_password") {
-			workingstring = Config._templates.warp_incorrect_password._;
+			workingstring = OldConfig._templates.warp_incorrect_password._;
 		}
 		
 		if(templateOption == "create_warp_denied_badrank") {
-			workingstring = Config._templates.create_warp_denied_badrank._;
+			workingstring = OldConfig._templates.create_warp_denied_badrank._;
 		}
 		
 		if(templateOption == "create_warp_denied_badterritory") {
-			workingstring = Config._templates.create_warp_denied_badterritory._;
+			workingstring = OldConfig._templates.create_warp_denied_badterritory._;
 		}
 		
 		if(templateOption == "warps_reached_max") {
-			workingstring = Config._templates.warps_reached_max._;
+			workingstring = OldConfig._templates.warps_reached_max._;
 		}
 		
 		if(templateOption == "warps_already_exists") {
-			workingstring = Config._templates.warps_already_exists._;
+			workingstring = OldConfig._templates.warps_already_exists._;
 		}
 		
 		if(templateOption == "faction_need") {
-			workingstring = Config._templates.faction_need._;
+			workingstring = OldConfig._templates.faction_need._;
 		}
 		
 		
