5a66857ce90f4fdfe11b8f4e6b7a839dfcdffcba
MarkehMe <mark@markeh.me>
2014-06-13 22:01:45 +1000

    Removing old comments, force all commands to enable by default now, removing reload command (no longer required)

--- src/markehme/factionsplus/FactionsPlusCommandManager.java
+++ src/markehme/factionsplus/FactionsPlusCommandManager.java
@@ -14,7 +14,6 @@ import markehme.factionsplus.Cmds.CmdJail;
 import markehme.factionsplus.Cmds.CmdListWarps;
 import markehme.factionsplus.Cmds.CmdMoneyTop;
 import markehme.factionsplus.Cmds.CmdPowSettings;
-import markehme.factionsplus.Cmds.CmdReloadFP;
 import markehme.factionsplus.Cmds.CmdRemoveRule;
 import markehme.factionsplus.Cmds.CmdRemoveWarp;
 import markehme.factionsplus.Cmds.CmdRules;
@@ -26,25 +25,11 @@ import markehme.factionsplus.Cmds.CmdUnban;
 import markehme.factionsplus.Cmds.CmdUnsetJail;
 import markehme.factionsplus.Cmds.CmdWarp;
 import markehme.factionsplus.Cmds.FPCommand;
-import markehme.factionsplus.config.Config;
 import markehme.factionsplus.extras.LWCBase;
 
 import com.massivecraft.factions.Factions;
 import com.massivecraft.factions.cmd.FCommand;
 import com.massivecraft.mcore.cmd.MCommand;
-
-
-
-/*
-	/f warp [name]	Warps to the Faction warp!	factionsplus.warp
-	/f createwarp [name]	Creates a Faction warp, these can only be created in faction land	factionsplus.createwarp
-	/f deletewarp [name]	Removes a Faction warp, these can only be created in faction land	factionsplus.deletewarp
-	/f removewarp [name]	Alias of deletewarp	factionsplus.deletewarp
-	/f setjail	Sets the jail location, this can only be set in faction land	factionsplus.setjail
-	/f jail [name]	Sends a player to jail!	factionsplus.jail
-	/f unjail [name]	Removes a player from jail!	factionsplus.jail
-
- */
 
 public class FactionsPlusCommandManager {
 	FactionsPlusCommandManager FactionsPlusCommandManager;
@@ -52,48 +37,35 @@ public class FactionsPlusCommandManager {
 	
 	public static void setup() {
 		// Warp Commands 
-		if(Config._warps.enabled._) {
-			addSC(new CmdAddWarp()); 
-			addSC(new CmdRemoveWarp());
-			addSC(new CmdWarp());
-			addSC(new CmdListWarps());
-		}
+		addSC(new CmdAddWarp()); 
+		addSC(new CmdRemoveWarp());
+		addSC(new CmdWarp());
+		addSC(new CmdListWarps());
 		
 		// Jail Commands
-		if(Config._jails.enabled._) {
-			addSC(new CmdSetJail());
-			addSC(new CmdUnsetJail());
-			addSC(new CmdJail());
-			addSC(new CmdUnJail());
-		}
+		addSC(new CmdSetJail());
+		addSC(new CmdUnsetJail());
+		addSC(new CmdJail());
+		addSC(new CmdUnJail());
+
 		// General Commands
-		if(Config._announce.enabled._) {
-			addSC(new CmdAnnounce());
-		}
-		
-		if(Config._peaceful.officersCanToggleState._ || Config._peaceful.membersCanToggleState._ || Config._peaceful.leadersCanToggleState._) {
-			addSC(new CmdToggleState());
-		}
+		addSC(new CmdAnnounce());
+
+		addSC(new CmdToggleState());
 		
 		// Faction Bans
-		if(Config._banning.enabled._) {
-			addSC(new CmdBan());
-			addSC(new CmdUnban());
-		}
+		addSC(new CmdBan());
+		addSC(new CmdUnban());
 
 		// Faction Rules
-		if(Config._rules.enabled._) {
-			addSC(new CmdSetRule());
-			addSC(new CmdRemoveRule());
-			addSC(new CmdRules());
-		}
+		addSC(new CmdSetRule());
+		addSC(new CmdRemoveRule());
+		addSC(new CmdRules());
 		
 		// Region based Commands
 		//addSC(new CmdPlot());
 
-		if (Config._economy.isHooked()){
-			Factions.get().getOuterCmdFactions().cmdFactionsMoney.addSubCommand(new CmdMoneyTop());
-		}
+		Factions.get().getOuterCmdFactions().cmdFactionsMoney.addSubCommand(new CmdMoneyTop());
 		
 		// New Admin commands 
 		addSC(new CmdFactionHome());
@@ -107,9 +79,7 @@ public class FactionsPlusCommandManager {
 		if(LWCBase.isLWCPluginPresent()) {
 			addSC(new CmdClearLocks());
 		}
-		
-		addSC(new CmdReloadFP());
-		
+				
 //		Bridge.factions.addSubCommand(P.p.cmdBase.cmdPower, new CmdPowPow());
 		addSC(new CmdPowSettings());
 		
@@ -125,6 +95,7 @@ public class FactionsPlusCommandManager {
 		}
 		
 	}
+	
 	
 	public static void disableSubCommands() throws Exception {
 		// Fetch all the sub commands
