5a1a6776b66361a7d16fed618708311c2362564f
MarkehMe <mark@markeh.me>
2014-06-13 22:48:57 +1000

    New Req: RulesEnabled

---
+++ src/markehme/factionsplus/Cmds/req/ReqJailsEnabled.java
@@ -0,0 +1,34 @@
+package markehme.factionsplus.Cmds.req;
+
+import markehme.factionsplus.MCore.FPUConf;
+
+import org.bukkit.command.CommandSender;
+
+import com.massivecraft.factions.entity.UPlayer;
+import com.massivecraft.mcore.cmd.MCommand;
+import com.massivecraft.mcore.cmd.req.ReqAbstract;
+
+public class ReqJailsEnabled extends ReqAbstract {
+	private static final long serialVersionUID = 1L;
+
+	// -------------------------------------------- //
+	// INSTANCE & CONSTRUCT
+	// -------------------------------------------- //
+
+	private static ReqJailsEnabled i = new ReqJailsEnabled();
+	public static ReqJailsEnabled get() { return i; }
+
+	// -------------------------------------------- //
+	// OVERRIDE
+	// -------------------------------------------- //
+
+	@Override
+	public boolean apply(CommandSender sender, MCommand command) {
+		return FPUConf.get(UPlayer.get(sender).getUniverse()).jailsEnabled;
+	}
+
+	@Override
+	public String createErrorMessage(CommandSender sender, MCommand command) {
+		return "Jails are not enabled.";
+	}
+}
