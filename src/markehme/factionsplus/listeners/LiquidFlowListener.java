9062903adefaf96c1766215febde8ea1d0441788
MarkehMe <mark@markeh.me>
2014-06-13 23:45:54 +1000

    MCore support + additional configuration option

--- src/markehme/factionsplus/listeners/LiquidFlowListener.java
+++ src/markehme/factionsplus/listeners/LiquidFlowListener.java
@@ -1,5 +1,7 @@
 package markehme.factionsplus.listeners;
 
+import markehme.factionsplus.MCore.FPUConf;
+
 import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
@@ -11,30 +13,41 @@ import com.massivecraft.factions.entity.BoardColls;
 import com.massivecraft.mcore.ps.PS;
 
 
-// Warning: REALLY high-resource use check that prevents lava flow between different faction owned territories (say good bye to cobble monsters)
-//			probably best to only be used on servers that can handle it. 
-
+/**
+ * This stops liquids flowing into other Factions, it can be intensive sometimes so
+ * it should only be used on servers that can handle it 
+ *
+ */
 public class LiquidFlowListener implements Listener {
-		
+	
 	@EventHandler(priority=EventPriority.HIGHEST)
 	public static void onBlockFromTo(BlockFromToEvent event) {
 		
-		Chunk fromchunk 	= event.getBlock().getChunk();
-		Chunk tochunk 		= event.getToBlock().getChunk();
+		if(FPUConf.isDisabled(event.getBlock().getLocation())) return;
 		
-		if( (!((event.getBlock().getTypeId() == 10 ) || ( event.getBlock().getTypeId() == 11 ) ) )
+		Chunk fromchunk = event.getBlock().getChunk();
+		Chunk tochunk = event.getToBlock().getChunk();
+		
+		// Ensure it is water or lava, and is not just inside the same chunk
+		if((!((event.getBlock().getType() == Material.WATER) || (event.getBlock().getType() == Material.LAVA)))
 				|| fromchunk == tochunk ) {
 			return;
 		}
 		
-		PS fromLoc 			= 		PS.valueOf(event.getBlock().getLocation());
-		PS toLoc 			= 		PS.valueOf(event.getToBlock().getLocation());
-			
-		if( BoardColls.get().getFactionAt( fromLoc ) != BoardColls.get().getFactionAt( toLoc ) ) {
+		PS fromLoc = PS.valueOf(event.getBlock().getLocation());
+		PS toLoc = PS.valueOf(event.getToBlock().getLocation());
+		
+		if(!FPUConf.get(fromLoc).fixes.containsKey("crossBorderLiquidFlowBlock")) return;
+		if(!FPUConf.get(fromLoc).fixes.get("crossBorderLiquidFlowBlock")) return;
+		
+		// Check if the chunks we are switching between are different factions 
+		if(BoardColls.get().getFactionAt(fromLoc) != BoardColls.get().getFactionAt(toLoc)) {
 			event.setCancelled(true);
 			
-			// Replace border with cobblestone to prevent major lagging
-			event.getBlock().setType(Material.COBBLESTONE);
+			// This is enabled by default to lower the chance of lag 
+			if(FPUConf.get(fromLoc).fixes.get("crossBorderLiquidFlowBlock"))  {
+				event.getBlock().setType(Material.COBBLESTONE);
+			}
 		}
 	}
 }
