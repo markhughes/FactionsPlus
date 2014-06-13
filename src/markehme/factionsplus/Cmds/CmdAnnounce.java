0ec5c75f90b63be5834f7d36d58fa6fa8e887bba
MarkehMe <mark@markeh.me>
2014-06-13 22:38:19 +1000

    MCore Support added, universe support

--- src/markehme/factionsplus/Cmds/CmdAnnounce.java
+++ src/markehme/factionsplus/Cmds/CmdAnnounce.java
@@ -2,105 +2,74 @@ package markehme.factionsplus.Cmds;
 
 
 
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import markehme.factionsplus.FactionsPlus;
-import markehme.factionsplus.FactionsPlusTemplates;
 import markehme.factionsplus.Utilities;
-import markehme.factionsplus.config.Config;
+import markehme.factionsplus.Cmds.req.ReqAnnouncementsEnabled;
+import markehme.factionsplus.MCore.FactionData;
+import markehme.factionsplus.MCore.FactionDataColls;
+import markehme.factionsplus.MCore.LConf;
+import markehme.factionsplus.MCore.FPUConf;
+import markehme.factionsplus.util.FPPerm;
 
-import org.bukkit.ChatColor;
 
 import com.massivecraft.factions.cmd.req.ReqFactionsEnabled;
 import com.massivecraft.factions.cmd.req.ReqHasFaction;
-import com.massivecraft.factions.entity.Faction;
+import com.massivecraft.mcore.cmd.req.ReqHasPerm;
 import com.massivecraft.mcore.cmd.req.ReqIsPlayer;
 import com.massivecraft.mcore.util.Txt;
 
 public class CmdAnnounce extends FPCommand {
 	public CmdAnnounce() {
-		this.aliases.add( "announce" );
+		
+		this.aliases.add("announce");
 		
 		this.fpidentifier = "announce";
 		
-		this.requiredArgs.add( "message" );
+		this.requiredArgs.add("message");
+		
 		this.errorOnToManyArgs = false;
 		
-		this.addRequirements( ReqFactionsEnabled.get() );
-		this.addRequirements( ReqIsPlayer.get() );
-		this.addRequirements( ReqHasFaction.get() );
+		this.addRequirements(ReqFactionsEnabled.get());
+		this.addRequirements(ReqIsPlayer.get());
+		this.addRequirements(ReqHasFaction.get());
+		this.addRequirements(ReqAnnouncementsEnabled.get());
+		 
+		this.addRequirements(ReqHasPerm.get(FPPerm.ANNOUNCE.node));
 		
-		this.setHelp("sends an announcment to your Faction");
-		this.setDesc("sends an announcment to your Faction");
+		this.setHelp(LConf.get().cmdDescAnnounce);
+		this.setDesc(LConf.get().cmdDescAnnounce);
+		
 	}
 	
 	@Override
 	public void performfp() {
-		if(!FactionsPlus.permission.has(sender, "factionsplus.announce")) {
-			sender.sendMessage(ChatColor.RED + "No permission!");
+		// Ensure there is a high enough ranking
+		if(!FPUConf.get(usender.getUniverse()).whoCanAnnounce.get(usender.getRole())) {
+			msg(Txt.parse(LConf.get().announcementNotHighEnoughRankingToSet));
 			return;
 		}
 		
-		String message 				= Txt.implode(args, " ").replaceAll("(&([a-f0-9]))", "& $2");
-		
-		Faction currentFaction 		= usender.getFaction();
-
-		if (! 
-				( (Config._announce.leadersCanAnnounce._ && Utilities.isLeader(usender)) 
-				||(Config._announce.officersCanAnnounce._ && Utilities.isOfficer(usender)) ) 
-		) {
-			sender.sendMessage(ChatColor.RED + "Sorry, your ranking is not high enough to do that!");
-			return;
-			
-		} 
-		
-		if(Config._economy.costToAnnounce._ > 0.0d) {
-			// TODO: move to pay for command thingy 
-			if ( !Utilities.doFinanceCrap( Config._economy.costToAnnounce._, "an announcement", usender ) ) {
+		if(FPUConf.get(usender.getUniverse()).economyCost.get("announce") > 0.0) {
+			if(!Utilities.doCharge(FPUConf.get(usender.getUniverse()).economyCost.get("announce"), usender)) {
+				msg(Txt.parse(LConf.get().announcementCanNotAfford, FPUConf.get(usender.getUniverse()).economyCost.get("announce")));
 				return;
 			}
 		}
 		
-		String[] argsa = { sender.getName(), message };
-			
-		String formatedAnnouncement = FactionsPlusTemplates.Go("announcement_message", argsa);
+		// Get the message to be announced
+		String message = Txt.implode(args, " ").replaceAll("(&([a-f0-9]))", "& $2");
 		
-		currentFaction.msg(formatedAnnouncement);
+		// Store the announcement data in their FactionData before we apply formatting 
+		FactionData fData = FactionDataColls.get().getForUniverse(usender.getUniverse()).get(usenderFaction.getId());
+		fData.announcement = message;
 		
-		DataOutputStream announceWrite = null;
-		
-		try {
-			
-			File fAF = new File(Config.folderAnnouncements, usender.getFactionId());
-			if(!fAF.exists()) {
-				fAF.createNewFile();
-			}
-			formatedAnnouncement = "Previously, " + formatedAnnouncement;
-			announceWrite = new DataOutputStream(new FileOutputStream(fAF, false));
-			announceWrite.write(formatedAnnouncement.getBytes());
-			
-		} catch (Exception e) {
-			
-			e.printStackTrace();
-		
-			sender.sendMessage( "[FactionsPlus] Failed to set announcement (Internal error -21)" );
-			
-			return;
-			
-		} finally {
-			if (null != announceWrite) {
-				try {
-					announceWrite.close();
-				} catch ( IOException e ) {
-					e.printStackTrace();
-				}
-			}
+		// If colours are enabled for announcements, we will parse it
+		if(FPUConf.get(usender.getUniverse()).allowColoursInAnnouncements) {
+			message = Txt.parse(message);
 		}
-
-
+		
+		// Announce it to the Faction
+		usenderFaction.msg(Txt.parse(LConf.get().announcementNotify, sender.getName(), message));
+		
 	}
 
 }
