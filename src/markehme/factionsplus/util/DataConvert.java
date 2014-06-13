e571548fdcde19e37324dcdeea8b6bb03701e10c
MarkehMe <mark@markeh.me>
2014-06-13 23:24:07 +1000

    DataConversion update

--- src/markehme/factionsplus/util/DataConvert.java
+++ src/markehme/factionsplus/util/DataConvert.java
@@ -5,7 +5,7 @@ import org.bukkit.ChatColor;
 import com.massivecraft.factions.Rel;
 
 import markehme.factionsplus.FactionsPlus;
-import markehme.factionsplus.MCore.UConf;
+import markehme.factionsplus.MCore.FPUConf;
 import markehme.factionsplus.config.OldConfig;
 
 public class DataConvert {
@@ -17,33 +17,33 @@ public class DataConvert {
 		if(OldConfig._jails.enabled._) {
 			FactionsPlus.info(			ChatColor.WHITE + "[Convert] - Jails");
 			
-			UConf.get("default").jailsEnabled = true;
+			FPUConf.get("default").jailsEnabled = true;
 			
 			if(OldConfig._jails.leadersCanSetJails._) {
 				updated("_jails.leadersCanSetJails._", "whoCanSetJails[Rel.LEADER]");
 				
-				UConf.get("default").whoCanSetJails.remove(Rel.LEADER);
-				UConf.get("default").whoCanSetJails.put(Rel.LEADER, true);
+				FPUConf.get("default").whoCanSetJails.remove(Rel.LEADER);
+				FPUConf.get("default").whoCanSetJails.put(Rel.LEADER, true);
 			}
 
 			if(OldConfig._jails.officersCanSetJails._) {
 				updated("_jails.officersCanSetJails._", "whoCanSetJails[Rel.OFFICER]");
 				
-				UConf.get("default").whoCanSetJails.remove(Rel.OFFICER);
-				UConf.get("default").whoCanSetJails.put(Rel.OFFICER, true);
+				FPUConf.get("default").whoCanSetJails.remove(Rel.OFFICER);
+				FPUConf.get("default").whoCanSetJails.put(Rel.OFFICER, true);
 			}
 
 			if(OldConfig._jails.membersCanSetJails._) {
 				updated("_jails.officersCanSetJails._", "whoCanSetJails[Rel.MEMBER]");
 				
-				UConf.get("default").whoCanSetJails.remove(Rel.MEMBER);
-				UConf.get("default").whoCanSetJails.put(Rel.MEMBER, true);
+				FPUConf.get("default").whoCanSetJails.remove(Rel.MEMBER);
+				FPUConf.get("default").whoCanSetJails.put(Rel.MEMBER, true);
 			}
 			
 			if(OldConfig._jails.canJailOnlyIfIssuerIsInOwnTerritory._) {
 				updated("_jails.canJailOnlyIfIssuerIsInOwnTerritory._", "mustBeInOwnTerritoryToJail");
 				
-				UConf.get("default").mustBeInOwnTerritoryToJail = true;
+				FPUConf.get("default").mustBeInOwnTerritoryToJail = true;
 			}
 			
 			
