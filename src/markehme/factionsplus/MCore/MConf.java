68cd92d69441364848639dd9dffa3e99045e254c
MarkehMe <mark@markeh.me>
2014-06-13 23:24:32 +1000

    Adding debug and scoreboard update option

--- src/markehme/factionsplus/MCore/MConf.java
+++ src/markehme/factionsplus/MCore/MConf.java
@@ -1,11 +1,6 @@
 package markehme.factionsplus.MCore;
 
-
-import java.util.Map;
-
 import com.massivecraft.mcore.store.Entity;
-import com.massivecraft.mcore.util.MUtil;
-
 
 public class MConf extends Entity<MConf> {
 	
@@ -23,16 +18,15 @@ public class MConf extends Entity<MConf> {
 	// Should we enable FactionsPlus at all?
 	public Boolean enabled = true;
 	
+	// Show debug informations
+	public Boolean debug = true;
+	
 	// Enable update checks?
 	public Boolean updateCheck = true;
 	
 	// Enable metrics?
 	public Boolean metrics = true;
 	
-	// Command aliases: /f <x> -> <y>
-	public Map<String, String> factionCommandAliases = MUtil.map(
-			"show", "/f f",
-			"power", "/f p"
-		);
-	
+	// Scoreboard update time
+	public long scoreboardUpdate = 5;
 }
