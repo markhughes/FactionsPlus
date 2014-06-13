49a7a37835fb970170bf952c1bf71affc906ef43
MarkehMe <mark@markeh.me>
2014-06-13 22:45:17 +1000

    New Req: Bans Enabled

---
+++ src/markehme/factionsplus/Cmds/req/ReqBansEnabled.java
@@ -0,0 +1,34 @@
+package markehme.factionsplus.Cmds.req;
+
+import markehme.factionsplus.MCore.FPUConf;
+
+import org.bukkit.command.CommandSender;
+
+import com.massivecraft.factions.entity.UPlayer;
+import com.massivecraft.mcore.cmd.MCommand;
+import com.massivecraft.mcore.cmd.req.ReqAbstract;
+
+public class ReqBansEnabled extends ReqAbstract {
+	private static final long serialVersionUID = 1L;
+
+	// -------------------------------------------- //
+	// INSTANCE & CONSTRUCT
+	// -------------------------------------------- //
+
+	private static ReqWarpsEnabled i = new ReqWarpsEnabled();
+	public static ReqWarpsEnabled get() { return i; }
+
+	// -------------------------------------------- //
+	// OVERRIDE
+	// -------------------------------------------- //
+
+	@Override
+	public boolean apply(CommandSender sender, MCommand command) {
+		return FPUConf.get(UPlayer.get(sender).getUniverse()).bansEnabled;
+	}
+
+	@Override
+	public String createErrorMessage(CommandSender sender, MCommand command) {
+		return "Bans are not enabled.";
+	}
+}
