dbd5e0187d77b34a1815662a6e4ff3fefc4d4b3b
MarkehMe <mark@markeh.me>
2014-06-14 23:40:27 +1000

    Making unjail work, has console and admin mode support

--- src/markehme/factionsplus/Cmds/CmdUnJail.java
+++ src/markehme/factionsplus/Cmds/CmdUnJail.java
@@ -1,7 +1,15 @@
 package markehme.factionsplus.Cmds;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+import markehme.factionsplus.EssentialsIntegration;
 import markehme.factionsplus.FactionsPlusJail;
 import markehme.factionsplus.Cmds.req.ReqJailsEnabled;
+import markehme.factionsplus.MCore.FactionData;
+import markehme.factionsplus.MCore.FactionDataColls;
 import markehme.factionsplus.MCore.LConf;
 import markehme.factionsplus.MCore.FPUConf;
 import markehme.factionsplus.util.FPPerm;
@@ -9,6 +17,7 @@ import markehme.factionsplus.util.FPPerm;
 
 import com.massivecraft.factions.cmd.req.ReqFactionsEnabled;
 import com.massivecraft.factions.cmd.req.ReqHasFaction;
+import com.massivecraft.factions.entity.UPlayer;
 import com.massivecraft.mcore.cmd.req.ReqHasPerm;
 import com.massivecraft.mcore.cmd.req.ReqIsPlayer;
 import com.massivecraft.mcore.util.Txt;
@@ -27,7 +36,6 @@ public class CmdUnJail extends FPCommand {
 		this.errorOnToManyArgs = false;
 		
 		this.addRequirements(ReqFactionsEnabled.get());
-		this.addRequirements(ReqIsPlayer.get());
 		this.addRequirements(ReqHasFaction.get());
 		this.addRequirements(ReqJailsEnabled.get());
 		
@@ -42,11 +50,49 @@ public class CmdUnJail extends FPCommand {
 	@Override
 	public void performfp() {
 		
-		if(!FPUConf.get(usender.getUniverse()).whoCanJail.get(usender.getRole())) {
-			msg(Txt.parse(LConf.get().jailsNotHighEnoughRanking));
+		OfflinePlayer jp = Bukkit.getPlayer(this.arg(0));
+		
+		if(jp == null) { 
+			jp = Bukkit.getOfflinePlayer(this.arg(0));
+		}
+		
+		if(jp == null) {
+			msg(Txt.parse(LConf.get().jailsPlayerNeverOnServer));
 			return;
 		}
 		
-		FactionsPlusJail.removeFromJail(this.arg(0), usender, true);
+		UPlayer ujPlayer = UPlayer.get(jp);
+		
+		if(sender instanceof Player && !usender.isUsingAdminMode()) {
+			if(!FPUConf.get(usender.getUniverse()).whoCanJail.get(usender.getRole())) {
+				msg(Txt.parse(LConf.get().jailsNotHighEnoughRanking));
+				return;
+			}
+			
+			if(usender.getFactionId() != ujPlayer.getFactionId()) {
+				msg(Txt.parse(LConf.get().jailsPlayerNotApartOfFaction, this.arg(0)));
+				return;
+			}
+		}
+		
+		FactionData jfData = FactionDataColls.get().getForUniverse(usender.getUniverse()).get(usenderFaction.getId());
+		
+		if(!jfData.isJailed(ujPlayer)) {
+			msg(Txt.parse(LConf.get().jailsPlayerNotInJail, this.arg(0)));
+			return;
+		}
+		
+		
+		msg(Txt.parse(LConf.get().jailsPlayerUnJailed, this.arg(0)));
+		
+		Location unteleportLocation = jfData.jailedPlayerIDs.get(jp.getUniqueId().toString()).asBukkitLocation();
+		jfData.jailedPlayerIDs.remove(jp.getUniqueId().toString());
+		
+		// If the player is online remove them from the jail
+		if(jp.isOnline()) {
+			if(!EssentialsIntegration.handleTeleport(jp.getPlayer(), unteleportLocation)) {
+				jp.getPlayer().teleport(unteleportLocation);
+			}
+		}
 	}
 }
