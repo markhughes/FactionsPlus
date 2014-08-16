c935eb223881d5cf8f9c50270ef10b5127747258
MarkehMe <mark@markeh.me>
2014-08-16 22:14:53 +1000

    Adding the new AnnouncementsSubListener

--- src/markehme/factionsplus/FactionsPlusListener.java
+++ src/markehme/factionsplus/FactionsPlusListener.java
@@ -8,6 +8,8 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 import com.massivecraft.factions.entity.UConf;
 import com.massivecraft.factions.entity.UPlayer;
@@ -22,6 +24,7 @@ import com.massivecraft.factions.entity.UPlayer;
 public class FactionsPlusListener implements Listener {
 	
 	AnimalDamageSubListener animalDamageSubListener = new AnimalDamageSubListener();
+	AnnoucementsSubListener annoucementsSubListener = new AnnoucementsSubListener();
 	
 	/**
 	 * isEnabled ensures Factions and FactionsPlus are enabled in that world.
@@ -35,9 +38,14 @@ public class FactionsPlusListener implements Listener {
 		return true;
 	}
 	
+	/*
+	 * Start all the event handlers 
+	 */
+	
 	@EventHandler(priority=EventPriority.LOWEST)
 	public void playerFishEvent(PlayerFishEvent event) {
 		if(!isEnabled(UPlayer.get(event.getPlayer()).getUniverse())) return;
+		//FPUConf fpuconf = FPUConf.get(UPlayer.get(event.getPlayer()).getUniverse());
 		
 		// Animal Damage Events
 		event = animalDamageSubListener.playerFishEvent(event);
@@ -46,8 +54,26 @@ public class FactionsPlusListener implements Listener {
 	@EventHandler(priority=EventPriority.LOWEST)
 	public void entityDamageByEntityEvent(EntityDamageByEntityEvent event) {
 		if(!isEnabled(event.getDamager().getWorld())) return;
+		//FPUConf fpuconf = FPUConf.get(event.getDamager().getWorld());
 		
 		// Animal Damage Events
 		event = animalDamageSubListener.entityDamageByEntityEvent(event);
+	}
+	
+	@EventHandler(priority=EventPriority.LOWEST)
+	public void playerJoinEvent(PlayerJoinEvent event) {
+		if(!isEnabled(event.getPlayer().getWorld())) return;
+		FPUConf fpuconf = FPUConf.get(event.getPlayer().getWorld());
+		
+		// Announcements Events
+		if(fpuconf.announcementsEnabled) event = annoucementsSubListener.playerJoinEvent(event);
+	}
+	
+	public void playerMoveEvent(PlayerMoveEvent event) {
+		if(!isEnabled(UPlayer.get(event.getPlayer()).getUniverse())) return;
+		FPUConf fpuconf = FPUConf.get(UPlayer.get(event.getPlayer()).getUniverse());
+		
+		if(fpuconf.announcementsEnabled) event = annoucementsSubListener.playerMoveEvent(event);
+
 	}
 }
