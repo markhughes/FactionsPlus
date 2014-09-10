e1d419080a81d7e10d4cca64c48be7f7555958b2
MarkehMe <mark@markeh.me>
2014-09-11 02:46:29 +1000

    Fix wilderness parser, checks Factions default IDs, comment additions, tidy up

--- src/main/java/markehme/factionsplus/extras/FType.java
+++ src/main/java/markehme/factionsplus/extras/FType.java
@@ -2,6 +2,7 @@ package markehme.factionsplus.extras;
 
 import com.massivecraft.factions.FFlag;
 import com.massivecraft.factions.entity.Faction;
+import com.massivecraft.factions.entity.UConf;
 
 /**
  * Anyone is free to use this FType enum as long as credit is 
@@ -39,7 +40,7 @@ public enum FType {
 	 * @param isWarZone
 	 * @param isWilderness
 	 */
-	private FType( final int id, final String niceName, final boolean isFaction, final boolean isSafezone, final boolean isWarZone, final boolean isWilderness ) {
+	private FType(final int id, final String niceName, final boolean isFaction, final boolean isSafezone, final boolean isWarZone, final boolean isWilderness) {
 		this._id = id;
 		this.iFaction = isFaction;
 		this.iSafezone = isSafezone;
@@ -55,14 +56,14 @@ public enum FType {
 	 * @return
 	 */
 	public static FType parse( String str ) {
-		if ( str == null ) return null;
+		if(str == null) return null;
 
 		String ty = str.trim().toLowerCase();
 		
-		if( ty == "safezone" ) return SAFEZONE;
-		if( ty == "warzone" ) return WARZONE;
-		if( ty == "safezone" ) return SAFEZONE;
-		if( ty == "faction" ) return FACTION;
+		if(ty == "wilderness") return WILDERNESS;
+		if(ty == "warzone") return WARZONE;
+		if(ty == "safezone") return SAFEZONE;
+		if(ty == "faction") return FACTION;
 
 		return null;
 	}
@@ -72,41 +73,41 @@ public enum FType {
 	 * @param faction
 	 * @return
 	 */
-	public static FType valueOf( Faction faction ) {
+	public static FType valueOf(Faction faction) {
 		
 		if(faction == null) return FType.WILDERNESS;
 		
 		// isNone always returns wilderness/none, easiest to detect 
-		if( faction.isNone() ) {
-			
+		if(faction.isNone()) {
 			return FType.WILDERNESS;
-			
 		}
 		
 		// If leader is not null (as in, there is a leader) it's a faction
 		// NOTE: Turns out, Permanent factions CAN have leaders (UConf: permanentFactionsDisableLeaderPromotion)
 		//		.. so, validate that it's not a permanent Faction
-		if( faction.getLeader() != null && ! faction.getFlag( FFlag.PERMANENT ) ) {
-			
+		if(faction.getLeader() != null && !faction.getFlag(FFlag.PERMANENT)) {
 			return FType.FACTION;
 			
 		}
 		
-		// If there is no PVP AND it is a PERMANENT Faction then it must be a safezone 
-		if( ! faction.getFlag( FFlag.PVP ) && faction.getFlag( FFlag.PERMANENT ) ) {
+		// If there is no PVP AND it is a PERMANENT Faction then it must be a safezone, or is
+		// the official safezone
+		if((!faction.getFlag(FFlag.PVP) && faction.getFlag(FFlag.PERMANENT)) ||
+			(faction.getId().equals(UConf.get(faction.getUniverse()).factionIdSafezone))) {
 			
 			return FType.SAFEZONE;
 			
 		}
 		
 		// If there is PvP, and it's not wilderness, and its permanent .. then it must be a warzone
-		if( faction.getFlag( FFlag.PVP ) && faction.getFlag( FFlag.PERMANENT ) ) {
+		if(faction.getFlag( FFlag.PVP ) && faction.getFlag(FFlag.PERMANENT) ||
+			(faction.getId().equals(UConf.get(faction.getUniverse()).factionIdWarzone))) {
 			
 			return FType.WARZONE;
 			
 		}
 		
-		// If FType is null, then Factions changed something and this library is out of date. 
+		// If FType is null, then Factions changed something and this library is out of date. #ohshit 
 		return null;		
 	}
 	
