1519792fd2005317455a282505236a84d2686961
MarkehMe <mark@markeh.me>
2014-09-16 14:37:00 +1000

    Adding CurrentScoreboard.None

--- src/main/java/markehme/factionsplus/scoreboard/CurrentScoreboard.java
+++ src/main/java/markehme/factionsplus/scoreboard/CurrentScoreboard.java
@@ -4,7 +4,7 @@ import markehme.factionsplus.MCore.FPUConf;
 
 public enum CurrentScoreboard {
 	
-	Power(1), Money(2), Members(3), Land(4);
+	None(0), Power(1), Money(2), Members(3), Land(4);
 	
 	CurrentScoreboard(int id) {
 		this.id = id;
@@ -13,12 +13,14 @@ public enum CurrentScoreboard {
 	int id = 0;
 	
 	public CurrentScoreboard getNext(FPUConf fpuconf) {
+		if(this.id == 0) return fpuconf.scoreboardDefault;
+		
 		if(this.id == 1) return Money.get(fpuconf);
 		if(this.id == 2) return Members.get(fpuconf);
 		if(this.id == 3) return Land.get(fpuconf);
 		if(this.id == 4) return Power.get(fpuconf);
 		
-		return null;
+		return Power;
 	}
 	
 	public CurrentScoreboard get(FPUConf fpuconf) {
