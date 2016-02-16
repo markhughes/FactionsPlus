43849bf05a8778525ae7f4ccdc77798a56e6e757
Mark Hughes <mark@markeh.me>
2016-02-17 09:16:54 +1100

    Configuration update

--- src/me/markeh/factionsplus/conf/Config.java
+++ src/me/markeh/factionsplus/conf/Config.java
@@ -2,6 +2,7 @@ package me.markeh.factionsplus.conf;
 
 import java.util.List;
 
+import me.markeh.factionsplus.FactionsPlus;
 import me.markeh.factionsplus.conf.obj.Configuration;
 import me.markeh.factionsplus.conf.obj.Option;
 import me.markeh.factionsplus.util.Utils;
@@ -22,79 +23,82 @@ public class Config extends Configuration<Config> {
 	// -----------------------
 
 	// ---- FactionsPlus Specific ---- //
-	@Option({"factionsplus", "debug", "Show debug messages?"}) 
+	@Option(section = "factionsplus", name = "debug", comment = "Show debug messages?") 
 	public Boolean debug = false; 
 	
-	@Option({"factionsplus", "metrics", "Enable metrics?"}) 
+	@Option(section = "factionsplus", name = "metrics", comment = "Enable metrics?") 
 	public Boolean metrics = true; 
 	
 	// ---- Jails ---- //
-	@Option({"jails", "enabled", "Should jails be enabled?"}) 
+	@Option(section = "jails", name = "enabled", comment = "Should jails be enabled?", pingOnUpdate = true) 
 	public Boolean enableJails = false; 
 	
-	@Option({"jails", "warmup", "Warm up time for the jail command (in seconds)"})
+	@Option(section = "jails", name = "warmup", comment = "Warm up time for the jail command (in seconds)") 
 	public Integer jailWarmUp = 5;
 	
-	@Option({"jails", "minimumSentence", "How long (in minutes) a player must be jailed for before they can be released."})
+	@Option(section = "jails", name = "minimumSentence", comment = "How long (in minutes) a player must be jailed for before they can be released") 
 	public Integer jailsMinimumSentence = 3; 
 	
 	// ---- Warps ---- //
-	@Option({"warps", "enabled", "Should warps be enabled?"})
+	@Option(section = "warps", name = "enabled", comment = "Should warps be enabled?") 
 	public Boolean enableWarps = false;
 	
-	@Option({"warps", "distanceCheckForEnemies", "Check the enemies (counted in blocks) in a distance and disallow using a warp if they're near by"})
+	@Option(section = "warps", name = "distanceCheckForEnemies", comment = "Check the enemies (counted in blocks) in a distance and disallow using a warp if they're near by") 
 	public Double warpsDistanceCheckForEnemies = 12.0;
 	
-	@Option({"warps", "warmUp", "Time (in seconds) before a player is teleported. Recommended minimum 6 seconds."})
+	@Option(section = "warps", name = "distanceCheckForEnemiesTerritoryOverride", comment = "If true, the above option is ignore if they're in their own land") 
+	public Boolean warpsDistanceCheckForEnemiesTerritoryOverride = true;
+	
+	@Option(section = "warps", name = "warmUp", comment = "Time (in seconds) before a player is teleported. Recommended minimum 6 seconds.") 
 	public Integer warpsWarmUp = 6;
 	
-	@Option({"warps", "announce", "Announce to Faction when warp is created or deleted"})
+	@Option(section = "warps", name = "announce", comment = "Announce to Faction when warp is created or deleted") 
 	public Boolean warpsAnnounce = true;
 	
 	// ---- Rules ---- //
-	@Option({"rules", "enabled", "Should rules be enabled?"})
+	@Option(section = "rules", name = "enabled", comment = "Should rules be enabled?") 
 	public Boolean enableRules = false;
 	
-	@Option({"rules", "max", "Maximum rules per faction"})
+	@Option(section = "rules", name = "max", comment = "Maximum rules per faction") 
 	public Integer rulesMax = 10;
 	
 	// ---- Announcements ---- //
-	@Option({"announcements", "enabled", "Enable announements?"})
+	@Option(section = "announcements", name = "enabled", comment = "Enable announements?") 
 	public Boolean enableAnnouncements = false;
 	
-	@Option({"announcements", "cooldown", "How long between announcement cooldowns in seconds"})
+	@Option(section = "announcements", name = "cooldown", comment = "How long between announcement cooldowns in seconds") 
 	public Integer announcementsCooldown = 5;
 	
 	// ---- Extras ---- //
-	@Option({"commandblock", "inRadiusOf", "Radius to check for enemy players to block a command"}) 
+	@Option(section = "commandblock", name = "inRadiusOf", comment = "Radius to check for enemy players to block a command") 
 	public Double commandBlockInRadiusOf = 0.0;
 	
-	@Option({"commandblock", "commands", "Commands to block"}) 
+	@Option(section = "commandblock", name = "commands", comment = "Commands to block") 
 	public List<String> commandBlockCommands = Utils.get().getList("examplecommand1", "examplecommand2");
 	
-	@Option({"wildernessregen", "enabled", "(unstable) Should we enable wilderness chunk regeneration?"}) 
+	@Option(section = "wildernessregen", name = "enabled", comment = "(unstable) Enable wilderness regeneration after a certain amount of time.", pingOnUpdate = true) 
 	public Boolean wildernessregenEnabled = false;
 	
-	@Option({"wildernessregen", "timer", "(unstable) After how many milliseconds of inactivity should they be regenerated?"}) 
+	@Option(section = "wildernessregen", name = "timer", comment = "After how many milliseconds of inactivity should they be regenerated?") 
 	public Long wildernessregenTimer = 43200000L;
 		
-	@Option({"wildernessregen", "useGriefManagementPlugin", "(unstable) If available, use a grief management plugin to roll back."}) 
+	@Option(section = "wildernessregen", name = "useGriefManagementPlugin", comment = "If available, use a grief management plugin to roll back.") 
 	public Boolean wildernessregenUseGriefManagementPlugin = true;
 
 	// ---- Scoreboard ---- //
-	@Option({"scoreboard", "enabled", "Enable the scoreboard?"}) 
+	@Option(section = "scoreboard", name = "enabled", comment = "(unstable) Enable the scoreboard?") 
 	public Boolean scoreboard_enabled = false;
 	
-	@Option({"scoreboard", "update", "Every x seconds we update the stats"}) 
+	@Option(section = "scoreboard", name = "update", comment = "Every x seconds we update the stats") 
 	public Integer scoreboard_update = 25;
 	
-	@Option({"scoreboard", "menus", "Which default menus should be enabled?"}) 
+	@Option(section = "scoreboard", name = "menus", comment = "Which default menus should be enabled?") 
 	public List<String> scoreboard_menus = Utils.get().getList("TopMembers", "TopPower");
 	
-	@Option({"scoreboard", "teams", "Add players to scoreboard teams "}) 
+	@Option(section = "scoreboard", name = "teams", comment = "Add players to scoreboard teams?") 
 	public Boolean scoreboard_teams = false;
 	
-	@Option({"scoreboard", "disabled", "Which worlds should all scoreboard features be disabled?"}) 
+	@Option(section = "scoreboard", name = "disabled", comment = "Which worlds should all scoreboard features be disabled?") 
 	public List<String> scoreboard_disabled = Utils.get().getList("gamelobby", "spleefarena1");
 	
 	// -----------------------
@@ -102,40 +106,40 @@ public class Config extends Configuration<Config> {
 	// -----------------------
 	
 	// ---- Chestshop Integration ---- //
-	@Option({"chestShop", "allowInWilderness", "Should we allow chest shops in wilderness?"}) 
+	@Option(section = "chestShop", name = "allowInWilderness", comment = "Should we allow chest shops in wilderness?") 
 	public Boolean chestShop_allowInWilderness = false;
 		
-	@Option({"chestShop", "allowInTerritory", "Should we allow chest shops in their territory?"}) 
+	@Option(section = "chestShop", name = "allowInTerritory", comment = "Should we allow chest shops in their territory?") 
 	public Boolean chestShop_allowInTerritory = false;
 	
-	@Option({"chestShop", "canUseEnemy", "Should we allow enemies to use chest shops?"}) 
+	@Option(section = "chestShop", name = "canUseEnemy", comment = "Should we allow enemies to use chest shops?") 
 	public Boolean chestShop_canUseEnemy = false;
 	
 	// ---- Disguise Integration ---- //
-	@Option({"disguiseIntegration", "allowInWilderness", "Should we allow disguises in wilderness?"}) 
+	@Option(section = "disguiseIntegration", name = "allowInWilderness", comment = "Should we allow disguises in wilderness?") 
 	public Boolean disguise_allowInWilderness = true;
 	
-	@Option({"disguiseIntegration", "allowEnemy", "Should we allow disgusies in enemy territory?"}) 
+	@Option(section = "disguiseIntegration", name = "allowEnemy", comment = "Should we allow disgusies in enemy territory?") 
 	public Boolean disguise_allowInEnemy = true;
 	
-	@Option({"disguiseIntegration", "allowTerritory", "Should we allow disgusies in their territory?"}) 
+	@Option(section = "disguiseIntegration", name = "allowTerritory", comment = "Should we allow disgusies in their territory?") 
 	public Boolean disguise_allowInTerritory = true;
 	
 	// ---- Cannons Integration ---- //
-	@Option({"cannonsIntegration", "allowInWilderness", "Allow building and using cannons in the wilderness?"}) 
+	@Option(section = "cannonsIntegration", name = "allowInWilderness", comment = "Allow building and using cannons in the wilderness?") 
 	public Boolean cannons_allowInWilderness = true;
 	
-	@Option({"cannonsIntegration", "allowInTerritory", "Allow building and using cannons in claimed territories?"}) 
+	@Option(section = "cannonsIntegration", name = "allowInTerritory", comment = "Allow building and using cannons in claimed territories?") 
 	public Boolean cannons_allowInTerritories = true;
 	
 	// ---- Showcase Integration ---- //
-	@Option({"showcase", "allowInWilderness", "Should we allow showcases in wilderness?"}) 
+	@Option(section = "showcase", name = "allowInWilderness", comment = "Should we allow showcases in wilderness?") 
 	public Boolean showcase_allowInWilderness = false;
 			
-	@Option({"showcase", "allowInTerritory", "Should we allow showcases in their territory?"}) 
+	@Option(section = "showcase", name = "allowInTerritory", comment = "Should we allow showcases in their territory?") 
 	public Boolean showcase_allowInTerritory = false;
 		
-	@Option({"showcase", "canUseEnemy", "Should we allow enemies to use showcases?"}) 
+	@Option(section = "showcase", name = "canUseEnemy", comment = "Should we allow enemies to use showcases?") 
 	public Boolean showcase_canUseEnemy = false;
 		
 	// -----------------------
@@ -147,5 +151,14 @@ public class Config extends Configuration<Config> {
 		if (instance == null) instance = new Config();
 		
 		return instance;
+	}	
+	
+	@Override
+	public final void postUpdatePing() {
+		// On configuration update, we will remanage our listeners
+		// just to ensure we only have the listeners enabled that
+		// we need :-) 
+		FactionsPlus.get().manageListeners();
 	}
+	
 }
